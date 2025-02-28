b6fc389a5879a09311cd00e2e9a4086991931663
ethan <ethanjupi@gmail.com>
2025-02-28 08:06:31 -0600

    Server, mongo swagger

---
+++ configs/swagger.js
@@ -0,0 +1,44 @@
+import swaggerJSDoc from "swagger-jsdoc"
+import swaggerUi from "swagger-ui-express"
+
+const options ={
+    swaggerDefinition:{
+        openapi: "3.0.0",
+        info:{
+            title: "InterferAPI",
+            version: "1.0.0",
+            description: "",
+            contact:{
+                name: "Ethan Juarez",
+                email: "ejuarez-2020269@kinal.edu.gt"
+            }
+        },
+        servers:[
+            {
+                url: "http://127.0.0.1:3001/interferApi/v1"
+            }
+        ],
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: "http",
+                    scheme: "bearer",
+                    bearerFormat: "JWT"
+                }
+            }
+        },
+        security: [
+            {
+                bearerAuth: []
+            }
+        ]
+    },
+    apis:[
+        "./src/user/user.routes.js",
+        "./src/auth/auth.routes.js"
+    ]
+}
+
+const swaggerDocs = swaggerJSDoc(options)
+
+export { swaggerDocs, swaggerUi }
